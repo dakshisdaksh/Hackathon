255525282f5f8ad382b5646ab4f539c89df10915
ananth243 <f20201797@goa.bits-pilani.ac.in>
2021-02-27 22:27:27 +0530

    Made the mongodb connect

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 const express= require('express');
+const mongoose= require('mongoose');
+const Del= require('./models/delivery');
 
 const app=express();
 
 app.set('view engine', 'ejs');
 
-app.listen('8080');
+const dbURI= "mongodb+srv://BITSPhoenix:@BITS123@cluster0.e8jal.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology:true})
+.then((result)=>app.listen('8080'))
+.catch((err)=> console.log(err));
+
+//middleware
+app.use(express.urlencoded({extended: true}));
+
+//Sandbox routes
+
+app.get('/add',(req, res)=>{
+    const del = new Del({
+        block: 'B',
+        number:'4',
+        room:'4j',
+        date:'6',
+    });
+    del.save()
+    .then((result)=>res.send(result))
+    .catch((err)=>console.log(err));
+});
+
+app.get('/all', (req, res)=>{
+    const del= new Del.find();
+    del.save()
+    .then((result)=> res.send(result))
+    .catch((err)=>console.log(err));
+});
+/*app.post('/past-orders', (req,res)=>{
+    const del= new Del(req.body);
+
+    del.save()
+    .then((result)=>{
+        res.redirect('/past-orders');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});*/
 
 app.get('/', (req, res)=>{
     res.render('index', {title: 'Homepage'});
@@ -16,6 +56,9 @@ app.get('/delivery-info', (req, res)=>{
 
 app.get('/items',(req,res)=>{
     res.render('items', {title: 'Contents'});
+});
+app.get('/past-orders', (req, res)=>{
+    res.render('past-orders', {title: 'Past Orders'});
 });
 
 app.use((req, res)=>{
---
+++ models/delivery.js
@@ -0,0 +1,32 @@
+const mongoose= require('mongoose');
+const Schema=mongoose.Schema;
+
+const delSchema= new Schema({
+    block: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    room: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    /*studentID: {
+        type: String,
+        required: true,
+    },
+    productID: {
+        type: String,
+        required: true,
+    },*/
+}, {timestamps: true});
+
+const Del= mongoose.model('Del', delSchema);
+module.exports= Del;
